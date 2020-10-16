2e252a6690eb8c7812a5381ab669893ec961042a
Gardarion <72802580+Gardarion@users.noreply.github.com>
2020-10-16 14:23:02 +0300

    Add files via upload

---
+++ 06. Act 5/0/77777777.js
@@ -0,0 +1,41 @@
+﻿
+function enter()
+{
+var code = document.getElementById("my_pass").value;
+var asap = "ДОМА";
+var pass = "ВСТР3Ч4";
+var dvas = "ПОДСТ4В4";
+var kras = "МОМ3НТ";
+var vras = "УБ1ЙСТВО";
+var dras = "ПР4ВОСУД13";
+var asas = "ХОНК";
+var sasa = "БРОСОК";
+if(code==pass)
+{location.href="../../08. Act 6/11. Act 6/08. Intermission 4/pages/7207.html";}
+else{
+if(code==dvas)
+{location.href="../../08. Act 6/11. Act 6/08. Intermission 4/pages/7233.html";}
+else{
+if(code==kras)
+{location.href="../../08. Act 6/11. Act 6/08. Intermission 4/pages/7248.html";}
+else{
+if(code==kras)
+{location.href="../../08. Act 6/11. Act 6/08. Intermission 4/pages/7248.html";}
+else{
+if(code==vras)
+{location.href="../../08. Act 6/11. Act 6/08. Intermission 4/pages/7286.html";}
+else{
+if(code==dras)
+{location.href="../../08. Act 6/11. Act 6/08. Intermission 4/pages/7302.html";}
+else{
+if(code==asas)
+{location.href="../../08. Act 6/11. Act 6/08. Intermission 4/pages/7320.html";}
+else{
+if(code==sasa)
+{location.href="../../08. Act 6/11. Act 6/08. Intermission 4/pages/7361.html";}
+else{
+if(code==asap)
+{location.href="../../08. Act 6/11. Act 6/08. Intermission 4/pages/7156.html";}
+else{
+location.href="777777777.html";}
+}}}}}}}}}
